902ef6a0f810790e1996dfda32951acce9ba31c1
JordanYanakiev <jordan.yanakiev@gmail.com>
2023-08-04 22:45:39 +0300

    Some changes

---
+++ PerfectBudgetApp/PerfectBudget.Test/BudgetTest.cs
@@ -0,0 +1,25 @@
+﻿using Moq;
+using PerfectBudgetApp.Data;
+using PerfectBudgetApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectBudget.Test
+{
+    [TestFixture]
+    public class BudgetTest
+    {
+        [Test] public void CreateBudgetTest()
+        {
+            var mockBudgetDb = new Mock<BudgetDbContext>();
+
+            var budget = new BudgetService();
+
+        }
+
+
+    }
+}
