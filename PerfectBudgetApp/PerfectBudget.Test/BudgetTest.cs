8b274111443ede1aadbdeb8fe50474b04050ac32
JordanYanakiev <jordan.yanakiev@gmail.com>
2023-08-06 14:52:12 +0300

    Building TestDb

--- PerfectBudgetApp/PerfectBudget.Test/BudgetTest.cs
+++ PerfectBudgetApp/PerfectBudget.Test/BudgetTest.cs
@@ -1,5 +1,9 @@
-﻿using Moq;
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using PerfectBudgetApp.Contracts;
 using PerfectBudgetApp.Data;
+using PerfectBudgetApp.Data.Models;
+using PerfectBudgetApp.Models.Budgets;
 using PerfectBudgetApp.Services;
 using System;
 using System.Collections.Generic;
@@ -12,18 +16,43 @@ namespace PerfectBudget.Test
     [TestFixture]
     public class BudgetTest
     {
-        private BudgetService budgetService;
+        private IBudgetService budgetService;
+        Mock mockBudgetDb;
 
         [SetUp] 
         public void SetUp() 
         {
-            var mockBudgetDb = new Mock<BudgetDbContext>();
-            budgetService = new BudgetService(mockBudgetDb.Object);
+            mockBudgetDb = new Mock<BudgetDbContext>();
+            mockBudgetDb.Setup(x => x.)
+            budgetService = new BudgetService(mockBudgetDb);
+
+            var budget = new Budget()
+            {
+                Id = Guid.NewGuid(),
+                Name = "TestBudget",
+                Amount = 250000
+            };
+
+
+            var userBudget = new UserBudget()
+            {
+                UserId = "00020000 - 0230 - 1234 - 5678 - 123000000000",
+                BudgetId = Guid.NewGuid(),
+                User = new User()
+                {
+                    Id = "00020000 - 0230 - 1234 - 5678 - 123000000000",
+                    NormalizedUserName = "Coco Maroko"
+                },
+                Budget = budget
+            };
         }
 
-        [Test] public void CreateBudgetTest()
+        [Test] 
+        public void CreateBudgetTest()
         {
-            
+            mockBudgetDb.Setup(x => x.CreateBudget());
+
+
 
 
         }
---
+++ PerfectBudgetApp/PerfectBudget.Test/TestDb.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Identity;
+using PerfectBudget.Data.Models;
+using PerfectBudgetApp.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfectBudget.Test
+{
+    public class TestDb
+    {
+
+        private string uniqueDbName;
+
+        public TestDb()
+        {
+            this.uniqueDbName = "PerfectBudget-TestDb-";
+            //this.SeedDatabase();
+        }
+
+        public Budget budget01 { get; private set; }
+        public Budget budget02 { get; private set; }
+        public BudgetExpense budgetExpense01 { get; private set; }
+        public BudgetExpense budgetExpense02 { get; private set; }
+        public Category categoryElectricity { get; private set; }
+        public Category categoryGroceries { get; private set; }
+        public Debt debt01 { get; private set; }
+        public Debt debt02 { get; private set; }
+        public DebtIssuer debtIssuer01 { get; private set; }
+        public DebtIssuer debtIssuer02 { get; private set; }
+        public DebtPayment debtPayment01 { get; private set; }
+        public DebtPayment debtPayment02 { get; private set; }
+        public DebtReceiver debtReceiver01 { get; private set;}
+        public DebtReceiver debtReceiver02 { get; private set;}
+        public DebtPaymentDebt debtPaymentDebt01 { get; private set; }
+        public DebtPaymentDebt debtPaymentDebt02 { get; private set; }
+        public Expense expense01 { get; private set; }
+        public Expense expense02 { get; private set; }
+        public ExpenseCategory expenseCategory01 { get; private set;}
+        public ExpenseCategory expenseCategory02 { get; private set;}
+        public Saving saving01 { get; private set; }
+        public Saving saving02 { get; private set; }
+        public IdentityUser user01 { get; private set; }
+        public IdentityUser user02 { get; private set; }
+        public UserBudget userBudget01 { get; private set; }
+        public UserBudget userBudget02 { get; private set; }
+        public UserDebt userDebt01 { get; private set; }
+        public UserDebt userDebt02 { get; private set; }
+        public UserExpense userExpense01 { get; private set; }
+        public UserExpense userExpense02 { get; private set; }
+        public UserExpense userExpense03 { get; private set; }
+        public UserExpense userExpense04 { get; private set; }
+        public UserExpense userExpense05 { get; private set; }
+        public UserExpense userExpense06 { get; private set; }
+        public UserSaving userSaving01 { get; private set; }
+        public UserSaving userSaving02 { get; private set; }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
