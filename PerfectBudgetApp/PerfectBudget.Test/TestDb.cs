52805f09d240f950afcd2e41dc453ee76dc37492
JordanYanakiev <jordan.yanakiev@gmail.com>
2023-08-07 12:56:46 +0300

    Some changes

--- PerfectBudgetApp/PerfectBudget.Test/TestDb.cs
+++ PerfectBudgetApp/PerfectBudget.Test/TestDb.cs
@@ -13,8 +13,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-
-
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.ComponentModel.DataAnnotations;
 
 namespace PerfectBudget.Test
 {
@@ -33,6 +33,7 @@ namespace PerfectBudget.Test
         public Budget budget02 { get; private set; }
         public BudgetExpense budgetExpense01 { get; private set; }
         public BudgetExpense budgetExpense02 { get; private set; }
+        public BudgetExpense budgetExpense03 { get; private set; }
         public Category categoryElectricity { get; private set; }
         public Category categoryGroceries { get; private set; }
         public Debt debt01 { get; private set; }
@@ -47,6 +48,7 @@ namespace PerfectBudget.Test
         public DebtPaymentDebt debtPaymentDebt02 { get; private set; }
         public Expense expense01 { get; private set; }
         public Expense expense02 { get; private set; }
+        public Expense expense03 { get; private set; }
         public ExpenseCategory expenseCategory01 { get; private set;}
         public ExpenseCategory expenseCategory02 { get; private set;}
         public Saving saving01 { get; private set; }
@@ -83,9 +85,90 @@ namespace PerfectBudget.Test
             var userStore = new UserStore<IdentityUser>(dbContext);
             var hasher = new PasswordHasher<IdentityUser>();
             var normalizer = new UpperInvariantLookupNormalizer();
-            var userManager = new UserManager<IdentityUser>(
-                userStore, null, hasher, null, null, normalizer, null, null, null);
+            var userManager = new UserManager<IdentityUser>(userStore, null, hasher, null, null, normalizer, null, null, null);
 
+            //Create user01
+            this.user01 = new IdentityUser()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "someemail@mail.bg",
+                NormalizedUserName = "SOMEEMAIL@MAIL.BG",
+                PasswordHash = "AQAAAAEAACcQAAAAEKJIzyYhT78c69h+srExBSUAF3SJaPu9PRpeDDXLviaUCrf6GUsQCYejGE933QseIQ=="
+            };
+            
+            //Create user02
+            this.user02 = new IdentityUser()
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "some.email@mail.bg",
+                NormalizedUserName = "SOME.EMAIL@MAIL.BG",
+                PasswordHash = "AQAAAAEAACcQAAAAEDAD9fgJshOHRusNzGKJBkm4NZj0fhrVpnLcf5kNUQG/toW2NDbzZyyAwc3XAc1JxQ=="
+            };
+
+            //Create budget01
+            this.budget01 = new Budget()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Pari za more",
+                Amount = 150000m
+            };
+
+            //create budget02
+            this.budget02 = new Budget()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Pari za hrana",
+                Amount = 20000m
+            };
+
+            //Create budgetExpense01
+            this.budgetExpense01 = new BudgetExpense()
+            {
+                ExpenseId = expense01.Id,
+                Expense = this.expense01,
+                BudgetId = this.budget01.Id,
+                Budget = this.budget01
+            };
+            
+            //Create budgetExpense02
+            this.budgetExpense02 = new BudgetExpense()
+            {
+                ExpenseId = expense02.Id,
+                Expense = this.expense02,
+                BudgetId = this.budget02.Id,
+                Budget = this.budget02
+            };
+            
+            //Create budgetExpense03
+            this.budgetExpense03 = new BudgetExpense()
+            {
+                ExpenseId = expense02.Id,
+                Expense = this.expense02,
+                BudgetId = this.budget02.Id,
+                Budget = this.budget02
+            };
+
+            //Create category01
+            this.categoryElectricity = new Category()
+            {
+                Name = "Electricity"
+            };
+
+            //Create category02
+            this.categoryGroceries = new Category()
+            {
+                Name = "Groceries"
+            };
+
+            //Create debt01 
+            this.debt01 = new Debt()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Pari za more",
+                LoanRequester = "John Doe",
+                LoanGiver = "John Doe II",
+                Amount = 1500m
+            };
         }
 
 
