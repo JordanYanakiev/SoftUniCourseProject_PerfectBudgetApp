79f326b5f78bb78fc0d88a4c15f394e0b4eb74d6
JordanYanakiev <jordan.yanakiev@gmail.com>
2023-08-10 00:16:41 +0300

    Some changes

--- PerfectBudgetApp/PerfectBudget.Test/TestDb.cs
+++ PerfectBudgetApp/PerfectBudget.Test/TestDb.cs
@@ -15,6 +15,7 @@ using System;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PerfectBudget.Test
 {
@@ -95,7 +96,10 @@ namespace PerfectBudget.Test
                 NormalizedUserName = "SOMEEMAIL@MAIL.BG",
                 PasswordHash = "AQAAAAEAACcQAAAAEKJIzyYhT78c69h+srExBSUAF3SJaPu9PRpeDDXLviaUCrf6GUsQCYejGE933QseIQ=="
             };
-            
+            userManager
+                .CreateAsync(this.user01, this.user01.UserName)
+                .Wait();
+
             //Create user02
             this.user02 = new IdentityUser()
             {
@@ -104,6 +108,9 @@ namespace PerfectBudget.Test
                 NormalizedUserName = "SOME.EMAIL@MAIL.BG",
                 PasswordHash = "AQAAAAEAACcQAAAAEDAD9fgJshOHRusNzGKJBkm4NZj0fhrVpnLcf5kNUQG/toW2NDbzZyyAwc3XAc1JxQ=="
             };
+            userManager
+                .CreateAsync(this.user02, this.user02.UserName)
+                .Wait();
 
             //Create budget01
             this.budget01 = new Budget()
@@ -112,6 +119,7 @@ namespace PerfectBudget.Test
                 Name = "Pari za more",
                 Amount = 150000m
             };
+            dbContext.Add(budget01);
 
             //create budget02
             this.budget02 = new Budget()
@@ -120,6 +128,7 @@ namespace PerfectBudget.Test
                 Name = "Pari za hrana",
                 Amount = 20000m
             };
+            dbContext.Add(budget02);
 
             //Create budgetExpense01
             this.budgetExpense01 = new BudgetExpense()
@@ -129,7 +138,8 @@ namespace PerfectBudget.Test
                 BudgetId = this.budget01.Id,
                 Budget = this.budget01
             };
-            
+            dbContext.Add(budgetExpense01);
+
             //Create budgetExpense02
             this.budgetExpense02 = new BudgetExpense()
             {
@@ -138,6 +148,7 @@ namespace PerfectBudget.Test
                 BudgetId = this.budget02.Id,
                 Budget = this.budget02
             };
+            dbContext.Add(budgetExpense02);
             
             //Create budgetExpense03
             this.budgetExpense03 = new BudgetExpense()
@@ -147,18 +158,21 @@ namespace PerfectBudget.Test
                 BudgetId = this.budget02.Id,
                 Budget = this.budget02
             };
+            dbContext.Add(budgetExpense03);
 
             //Create category01
             this.categoryElectricity = new Category()
             {
                 Name = "Electricity"
             };
+            dbContext.Add(categoryElectricity);
 
             //Create category02
             this.categoryGroceries = new Category()
             {
                 Name = "Groceries"
             };
+            dbContext.Add(categoryGroceries);
 
             //Create debt01 
             this.debt01 = new Debt()
@@ -169,6 +183,142 @@ namespace PerfectBudget.Test
                 LoanGiver = "John Doe II",
                 Amount = 1500m
             };
+            dbContext.Add(debt01);
+            
+            //Create debt02
+            this.debt02 = new Debt()
+            {
+                Id = Guid.NewGuid(),
+                Name = "Pari za planina",
+                LoanRequester = "John Doe",
+                LoanGiver = "John Doe II",
+                Amount = 500m
+            };
+            dbContext.Add(debt02);
+
+            //Create Debt Issuer 01
+            this.debtIssuer01 = new DebtIssuer()
+            {
+                DebtId = Guid.NewGuid(),
+                Debt  = debt01,
+                DebtIssuerId = user01.Id,
+                UserId  = user01
+            };
+            dbContext.Add(debtIssuer01);
+
+             //Create Debt Issuer 02
+            this.debtIssuer02 = new DebtIssuer()
+            {
+                DebtId = Guid.NewGuid(),
+                Debt  = debt02,
+                DebtIssuerId = user01.Id,
+                UserId  = user01
+            };
+            dbContext.Add(debtIssuer02);
+
+            //Create debt payment01
+            this.debtPayment01 = new DebtPayment()
+            {
+                Id = Guid.NewGuid(),
+                CreditInstallment = 1500m
+            };
+            dbContext.Add(debtPayment01);
+
+            
+            //Create debt payment02
+            this.debtPayment02 = new DebtPayment()
+            {
+                Id = Guid.NewGuid(),
+                CreditInstallment = 500m
+            };
+            dbContext.Add(debtPayment02);
+
+            //Create debtPaymentDebt01
+            this.debtPaymentDebt01 = new DebtPaymentDebt()
+            {
+                DebtId = debt01.Id,
+                Debt = debt01,
+                DebtInstallmentId = debtPayment01.Id,
+                DeptPayment = debtPayment01
+            };
+            dbContext.Add(debtPaymentDebt01);
+
+            //Create debtPaymentDebt02
+            this.debtPaymentDebt02 = new DebtPaymentDebt()
+            {
+                DebtId = debt02.Id,
+                Debt = debt02,
+                DebtInstallmentId = debtPayment02.Id,
+                DeptPayment = debtPayment02
+            };
+            dbContext.Add(debtPaymentDebt02);
+
+            //Create debtReceiver01
+            this.debtReceiver01 = new DebtReceiver()
+            {
+                DebtId = debt01.Id,
+                Debt = debt01,
+                DebtReceiverId = user02.Id,
+                UserId = user02
+            };
+            dbContext.Add(debtReceiver01);
+            
+            //Create debtReceiver02
+            this.debtReceiver02 = new DebtReceiver()
+            {
+                DebtId = debt02.Id,
+                Debt = debt02,
+                DebtReceiverId = user02.Id,
+                UserId = user02
+            };
+            dbContext.Add(debtReceiver02);
+
+            this.expense01 = new Expense()
+            {
+                Id = Guid.NewGuid(),
+                CategoryId = categoryElectricity.Id,
+                Category = categoryElectricity,
+                Amount = debtPayment01.CreditInstallment,
+                Description = "Tok",
+                BudgetId = budget01.Id,
+                Budget = budget01,
+                DateOfIssuedExpense = new DateTime(2023, 8, 15)
+            };
+            dbContext.Add(expense01);
+            
+            this.expense02 = new Expense()
+            {
+                Id = Guid.NewGuid(),
+                CategoryId = categoryGroceries.Id,
+                Category = categoryGroceries,
+                Amount = debtPayment01.CreditInstallment,
+                Description = "Bread and onion",
+                BudgetId = budget01.Id,
+                Budget = budget01,
+                DateOfIssuedExpense = new DateTime(2023, 8, 20)
+            };
+            dbContext.Add(expense02);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
 
 
